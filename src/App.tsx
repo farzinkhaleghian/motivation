1e0d3fde78ef689fc6f2356639796ef2128ebe07
jazzb-admin <108554050+jazzb-admin@users.noreply.github.com>
2023-10-07 23:26:30 -0500

    motivation quote

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const quotes = [
+  "You can do it, Diana!",
+  "Believe in yourself, Diana!",
+  "You're awesome, Diana!",
+  "Never give up, Diana!",
+  "Sky's the limit, Diana!",
+  "Stay focused, Diana!",
+  "You're capable of more than you know, Diana!",
+  "Be fearless, Diana!",
+  "Keep pushing forward, Diana!",
+  "You're doing great, Diana!"
+];
+
 function App() {
+  const [quote, setQuote] = useState(quotes[0]);
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome, Diana!</h1>
+        <p className="quote">{quote}</p>
+        <button onClick={getRandomQuote}>Get New Quote</button>
       </header>
     </div>
   );
